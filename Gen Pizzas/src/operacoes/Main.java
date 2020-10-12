b47114cdad64878231130cde111e0015adc1eea7
LAPTOP-E3S3ILG8\Diana <coelhodiana04@gmail.com>
2020-10-11 22:40:15 -0300

    Somando os códigos

--- Gen Pizzas/src/operacoes/Main.java
+++ Gen Pizzas/src/operacoes/Main.java
@@ -1,48 +1,45 @@
 package operacoes;
 
 import java.util.Scanner;
-
+	
 public class Main {
-	public static void cardapio()
+/*	public static void pular()
+	{
+		for(int p = 0; p <=20; p++)
+		{
+				System.out.println("\n");
+		}
+	}
+	*/
+
+	public static void cardapioPizza()
 	{
 		System.out.println
-				("---------------------------------------------------------------------------------"
+				("-----------------------------------------------------------------------------------------"
 				+"\n*Pizzas*\n"
-				+"Mussarela - mussarela, molho de tomate, oregano e rodelas de tomate - 20,00R$\n"
-				+"Fran Catu - frango desfiado, molho de tomate, catupiry e oregano    - 23,00R$\n"
-				+"Calabreza - calabreza, molho de tomate, cebola e oregano            - 25,00R$\n"
-				+"---------------------------------------------------------------------------------");
+				+"-----------------------------------------------------------------------------------------"
+				+"\nCod     Sabor       Ingredientes                                              Preços\n"
+				+"1 - 	Mussarela - mussarela, molho de tomate, oregano e rodelas de tomate - 21,00R$\n"
+				+"2 - 	Fran Catu - frango desfiado, molho de tomate, catupiry e oregano    - 23,00R$\n"
+				+"3 - 	Calabresa - calabreza, molho de tomate, cebola e oregano            - 25,00R$\n"
+				+"4 - Sair\n"
+				+"-----------------------------------------------------------------------------------------\n");
 	}
 	
-	public static void menu()
+	public static void cardapioBebida()
 	{
-		Scanner read = new Scanner(System.in);
-		int opInicio;
-		
-		//Menu inicial
-			System.out.println("Olá! O que faremos hoje?\n");
-			System.out.println("1_ Registrar um pedido\n");
-			System.out.println("2_ Ver lista de pedidos\n");
-			System.out.println("3_ Ver faturamento do dia\n");
-			System.out.println("Digite o número da opção para selecionar: ");
-			opInicio = read.nextInt();
-			
-			switch(opInicio) {
-			case 1:
-				cardapio();
-				break;
-			case 2:
-				System.out.println("Mostra lista dos pedidos\n\n");
-
-				break;
-			case 3:
-				System.out.println("Mostrar faturamento");
-
-				break;
-			default:
-				System.out.println("Ops! parece que você digitou um número inválido");
-			}
+		System.out.println
+				("-----------------------------------------------------------------------------------------"
+				+"\n*Bebidas*\n"
+				+"-----------------------------------------------------------------------------------------"
+				+"\nCod       Marca           Preços\n"
+				+"1 - 	  Dolly 2L     -  4,50R$\n"
+				+"2 - 	  Coca-Cola 2L -  6,00R$\n"
+				+"3 - 	  Agua 600ml   -  3,50R$\n"
+				+"4 - Sair\n"
+				+"-----------------------------------------------------------------------------------------\n");
 	}
+	
 	
 	public static void  main(String args[])
 	{
@@ -59,7 +56,123 @@ public class Main {
 			
 			switch(opInicio) {
 			case 1:
-				cardapio();
+				//Início pedido
+				
+				Scanner ler = new Scanner(System.in);
+				int opcPizza = 5, opcBebida = 5, qtd = 0, formaPgto;
+				double totalPizza = 0, totalBebida = 0, pagoCliente = 0;
+				double precos[] = {21,23,25,4.5,6,3.5};
+
+				while(opcPizza != 0)
+				{
+					cardapioPizza();
+					System.out.println("Digite o código da pizza desejada: ");
+					opcPizza = ler.nextInt();
+						
+					if(opcPizza == 1)
+					{
+						//System.out.println("");
+						//if(qtd == 1)
+						System.out.println("Pizza sabor *Mussarela* adicionar ao carrinho!");
+						totalPizza = totalPizza + precos[0];
+						opcPizza = 0;
+					}
+					else if(opcPizza == 2)
+					{
+						System.out.println("Pizza sabor *Fran Catu* adicionar ao carrinho!");
+						totalPizza = totalPizza + precos[1];
+						opcPizza = 0;
+					}
+					else if(opcPizza == 3)
+					{
+						System.out.println("Pizza sabor *Calabresa* adicionar ao carrinho!");
+						totalPizza = totalPizza + precos[2];
+						opcPizza = 0;
+					}
+					else if(opcPizza == 4)
+					{
+						opcPizza = 0;
+					}
+					else
+					{
+						System.out.println("Código Invalido! ");
+					}
+						
+					System.out.println("\nGostaria de adicionar mais pizzas? 1 - sim//0 - não");
+					opcPizza = ler.nextInt();
+				
+				}	
+				
+				while(opcBebida != 0)
+				{
+					cardapioBebida();
+					System.out.println("Digite o código da bebida desejada: ");
+					opcBebida = ler.nextInt();
+					
+					if(opcBebida == 1)
+					{
+						System.out.println("Dolly adicionado ao carrinho!");
+						totalBebida = totalBebida + precos[3];
+						opcBebida = 0;
+					}
+					else if(opcBebida == 2)
+					{
+						System.out.println("Coca-cola adicionada ao carrinho!");
+						totalBebida = totalBebida + precos[4];
+						opcBebida = 0;
+					}
+					else if(opcBebida == 3)
+					{
+						System.out.println("Água adicionada ao carrinho!");
+						totalBebida = totalBebida + precos[5];
+						opcBebida = 0;
+					}
+					else if(opcPizza == 4)
+					{
+						opcPizza = 0;
+					}
+					else
+					{
+						System.out.println("Código de bebida inválido!");
+					}
+					
+					System.out.println("\nGostaria de adicionar mais bebidas? 1 - sim//0 - não");
+					opcBebida = ler.nextInt();
+				}
+				
+				System.out.println("Compra finalizada");
+				 
+				System.out.println("Qual a forma de pagamento?");		
+				System.out.println("1. Dinheiro");
+				System.out.println("2. Cartão de Débito");
+				System.out.println("3. Cartão de Crédito");
+		 
+				formaPgto=ler.nextInt();
+		 
+				double total = totalPizza+totalBebida;
+				if(formaPgto==1) 
+				{
+		 
+					System.out.println("O valor da comanda é de "+(total*0.9));			
+					System.out.println("Qual o valor pago pelo cliente? ");
+					pagoCliente = ler.nextFloat();	
+					System.out.println("O troco é de "+(pagoCliente - (total*0.9)));
+				}
+				else if(formaPgto == 2) 
+				{
+					System.out.println("O valor da comanda é de "+total*0.95);
+				}
+				else if(formaPgto == 3) 
+				{
+					System.out.println("O valor da comanda é de "+ total);
+				}
+				else
+				{
+					System.out.println("Forma de pagamento não configurada");
+				}	
+				
+				//fim pedido
+				
 				break;
 			case 2:
 				System.out.println("Mostra lista dos pedidos\n\n");
@@ -72,8 +185,8 @@ public class Main {
 			default:
 				System.out.println("Ops! parece que você digitou um número inválido\n");
 				System.out.println("---------------------------------------------------------------------------------\n\n");
-				menu();
+				
 			}
-		
 	}
+
 }
